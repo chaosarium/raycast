e67fe73e0558b19648bafeae8b9d7c46a4277097
chaosarium <leonluleonlu@gmail.com>
2022-08-02 13:46:26 +0800

    obsidian capture idea

---
+++ obsidian/obsidian-capture-idea.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Required parameters: text
+# @raycast.schemaVersion 1
+# @raycast.title Obsidian Capture Idea
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.packageName obsidian-capture-idea
+# @raycast.icon ./assets/obsidian.png
+# @raycast.argument1 { "type": "text", "placeholder": "" }
+
+# Documentation:
+# @raycast.author chaosarium
+# @raycast.authorURL https://github.com/chaosarium
+
+import sys, os, datetime
+query = sys.argv[1]
+
+# set vault path
+OBSIDIAN_VAULT_PATH = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Zettelkasten"
+# file to manipulate within vault
+FILE_PATH = "100 Vault/Ideas Inbox.md"
+# format appended text
+insert_text = f"""
+
+%%captured {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}%%
+{query}
+
+---"""
+
+abs_path = os.path.expanduser(os.path.join(OBSIDIAN_VAULT_PATH, FILE_PATH))
+
+file = open(abs_path, "a")
+file.write(insert_text)
